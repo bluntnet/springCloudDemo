8f587d7095f5c95df99f68bdff3c01e1edc388ad
bluntnet <37933023@qq.com>
2021-05-30 12:52:57 +0800

    zuul 增加熔断的处理

---
+++ zuul/src/main/java/demo/zuul/MyZuulFallbackProvider.java
@@ -0,0 +1,57 @@
+package demo.zuul;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class MyZuulFallbackProvider implements FallbackProvider {
+    @Override
+    public String getRoute() {
+        return "*";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders header = new HttpHeaders();
+                header.setContentType(MediaType.APPLICATION_JSON);
+                return header;
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("暂时不可能，请稍候再试..".getBytes());
+            }
+
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return HttpStatus.OK.value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return HttpStatus.OK.getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+        };
+    }
+}

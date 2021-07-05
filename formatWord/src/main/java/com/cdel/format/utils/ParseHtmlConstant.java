3c21cb5fbbf0e8a193f8778dc29cdaac2bfd07d6
chenxinjie <chenxinjie@cdeledu.com>
2021-07-05 08:56:04 +0800

    习题的【答案】和【正确答案】都可以匹配 xiti 3

--- formatWord/src/main/java/com/cdel/format/utils/ParseHtmlConstant.java
+++ formatWord/src/main/java/com/cdel/format/utils/ParseHtmlConstant.java
@@ -1,5 +1,7 @@
 package com.cdel.format.utils;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ParseHtmlConstant {
@@ -19,6 +21,16 @@ public class ParseHtmlConstant {
     public static String TAG_TABLE = "table";
     public static String TAG_IMAGE = "img";
     public static Pattern REG_QUESTION = Pattern.compile("【(单|多)选题】[\\s\\S]*");
-    public static Pattern REG_RIGHT_ANSWER = Pattern.compile("【正确答案】[\\s\\S]*");
+    public static Pattern REG_RIGHT_ANSWER = Pattern.compile("【(正确)?答案】[\\s\\S]*");
+
+    public static void main(String[] args) {
+
+        String[] list = {"【正确答案】", "【答案】"};
+
+        Arrays.stream(list).forEach(temp -> {
+            Matcher matcher = REG_RIGHT_ANSWER.matcher(temp);
+            System.out.println(temp + REG_RIGHT_ANSWER.toString() +" ===" + matcher.matches());
+        });
+    }
 
 }
